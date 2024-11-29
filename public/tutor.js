a95f8763ca8f512e14851b1be868f3e08542d8c1
Mehar Khanna <imeharkhanna8@gmail.com>
2024-11-29 04:59:17 -0500

    Auto-deploy update Fri Nov 29 04:59:17 EST 2024

--- public/tutor.js
+++ public/tutor.js
@@ -18,6 +18,27 @@ console.log('tutor.js loaded');
                 }
             }
         };
+
+// Load highlight.js
+const highlightScript = document.createElement('script');
+highlightScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js';
+highlightScript.onload = () => {
+    console.log('Highlight.js loaded');
+    // Load common languages
+    ['cpp', 'python', 'javascript', 'java', 'csharp'].forEach(lang => {
+        const script = document.createElement('script');
+        script.src = `https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/${lang}.min.js`;
+        document.head.appendChild(script);
+    });
+};
+document.head.appendChild(highlightScript);
+
+// Load highlight.js CSS
+const highlightCSS = document.createElement('link');
+highlightCSS.rel = 'stylesheet';
+highlightCSS.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/vs2015.min.css';
+document.head.appendChild(highlightCSS);
+
 document.addEventListener("DOMContentLoaded", async () => {
     await window.envLoaded;
     
